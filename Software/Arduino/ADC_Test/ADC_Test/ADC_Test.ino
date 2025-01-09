411173240d881eab641175a3bef27654aaa905fa
theBrutzler <34459192+theBrutzler@users.noreply.github.com>
2025-01-09 23:10:10 +0100

    Update Software (VBAT in ADC ino and retro.go img)

--- Software/Arduino/ADC_Test/ADC_Test/ADC_Test.ino
+++ Software/Arduino/ADC_Test/ADC_Test/ADC_Test.ino
@@ -27,20 +27,36 @@
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/AnalogInput
 */
 
-int sensorPin1 = 6;   // select the input pin for the potentiometer
-int sensorPin2 = 7;   // select the input pin for the potentiometer
+int DISPLAY_BLK = 39;   // select the input pin for the potentiometer
+int LEFT_RIGHT_Pin = 6;   // select the input pin for the potentiometer
+int UP_DOWN_Pin = 7;   // select the input pin for the potentiometer
+int VBATPin = 4;   // select the input pin for the potentiometer
 int sensorValue1 = 0;  // variable to store the value coming from the sensor
 int sensorValue2 = 0;  // variable to store the value coming from the sensor
+int VBATValu = 0;  // variable to store the value coming from the sensor
+float VBATValu2 = 0;  // variable to store the value coming from the sensor
+float VBATPerc = 0;  // variable to store the value coming from the sensor
 
 void setup() {
-  // declare the ledPin as an OUTPUT:
   Serial.begin(115200);
+  pinMode(DISPLAY_BLK, OUTPUT);
+  digitalWrite(DISPLAY_BLK, LOW);
 }
 
 void loop() {
   // read the value from the sensor:
-  sensorValue1 = analogRead(sensorPin1);
-  sensorValue2 = analogRead(sensorPin2);
+  sensorValue1 = analogRead(LEFT_RIGHT_Pin);
+  sensorValue2 = analogRead(UP_DOWN_Pin);
+  VBATValu = analogRead(VBATPin);
+  VBATPerc = (((VBATValu) * 2.7f - 2000.f) / (2800.f - 2000.f) * 100.f);
+  VBATValu2 = ((VBATValu) * 2.7f * 0.001f);
+  
+  Serial.print("Value:");
+  Serial.print(VBATValu2);
+  Serial.print(",");  
+  Serial.print("Perc:");
+  Serial.print(VBATPerc);
+  Serial.print(",");
   Serial.print("UP_DOWN:");
   Serial.print(sensorValue2);
   Serial.print(",");
